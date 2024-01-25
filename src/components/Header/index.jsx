aa96fdd6197c212f4f84ec481ea84b1db85eea50
dannyp0930 <dannyp0930@onoffmix.com>
2024-01-25 21:43:29 +0900

    Initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <NavLink to="/">SH Portfolio</NavLink>
+        <NavLink to="/content">Content</NavLink>
+      </nav>
+    </header>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+const webpackMode = process.env.NODE_ENV;
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  mode: webpackMode,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename:
+      webpackMode === "production"
+        ? "[name].[contenthash].js"
+        : "[name].min.js",
+  },
+  optimization: {
+    minimizer:
+      webpackMode === "production"
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+            }),
+            new CssMinimizerPlugin(),
+          ]
+        : [],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+        resolve: {
+          extensions: ["", ".js", ".jsx"],
+        },
+      },
+      {
+        test: /\.?(js|jsx)$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          webpackMode === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "src/components"),
+      pages: path.resolve(__dirname, "src/pages"),
+      styles: path.resolve(__dirname, "src/styles"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      minify:
+        webpackMode === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename:
+        webpackMode === "production"
+          ? "[name].[contenthash].css"
+          : "[name].min.css",
+    }),
+    new Dotenv({
+      path: `.env.${webpackMode === "production" ? "prod" : "dev"}`,
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
