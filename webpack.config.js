b78afa06a65b0eddcf9d47145b893445e4fff89b
dannyp0930 <dannyp0930@gmail.com>
2024-01-25 21:56:23 +0900

    FEAT: github pages setting

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   mode: webpackMode,
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename:
       webpackMode === "production"
         ? "[name].[contenthash].js"
