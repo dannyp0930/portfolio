b17a4f3c700e8b1db90f070fb88bee0cc8715cc1
dannyp0930 <dannyp0930@onoffmix.com>
2024-02-01 21:01:17 +0900

    FEAT: route 수정

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,13 +5,10 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <NavLink className="desktop-tablet home" to="/">SH Portfolio</NavLink>
+        <NavLink className="desktop-tablet home" to="/portfolio">SH Portfolio</NavLink>
         <NavLink className="mobile home" to="/portfolio">SH</NavLink>
         <ul className="desktop-tablet">
-          <li><NavLink to="/portfolio/content">Content</NavLink></li>
-          <li><NavLink to="/portfolio/content">Content</NavLink></li>
-          <li><NavLink to="/portfolio/content">Content</NavLink></li>
-          <li><NavLink to="/portfolio/content">Content</NavLink></li>
+          <li><NavLink to="/portfolio/project">Project</NavLink></li>
         </ul>
       </nav>
     </header>
