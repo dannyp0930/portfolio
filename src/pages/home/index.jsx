d7cbebc2fe4572ab6ad57d95e347de1c73d52bd5
dannyp0930 <dannyp0930@onoffmix.com>
2024-02-01 21:04:28 +0900

    FEAT: 홈 섹션 분류

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,21 @@ import React from "react";
 export default function Home() {
   return (
     <article>
-      <h1>HOME</h1>
+      <section>
+         배너
+      </section>
+      <section>
+        기본정보(연락처)
+      </section>
+      <section>
+        설명
+      </section>
+      <section>
+        스택
+      </section>
+      <section>
+        경력 / 자격사항
+      </section>
     </article>
   );
 }
