77f565d6ced7ca59bec17b4121e65886ff789842
dannyp0930 <dannyp0930@onoffmix.com>
2024-02-14 23:33:42 +0900

    FEAT: footer 기본 형태

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,16 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="link-container">
+        <a href="https://github.com/dannyp0930" target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+      <h1>© 2024. Park Sang Hun. All rights reserved.</h1>
+    </footer>
+  );
 }
