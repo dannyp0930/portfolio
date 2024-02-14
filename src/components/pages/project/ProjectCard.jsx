81117560209b7750152ab83295d59d4429b92dbd
dannyp0930 <dannyp0930@onoffmix.com>
2024-02-14 23:28:16 +0900

    FEAT: project 날짜/조직 추가

--- src/components/pages/project/ProjectCard.jsx
+++ src/components/pages/project/ProjectCard.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { timeToDate } from "utils";
 
 export default function ProjectCard({ project }) {
   return (
     <div className="project-card">
-      <h1>{project.title}</h1>
+      <h1>{project.title}<span>{project.organization}</span></h1>
       <h4>{project.intro}</h4>
+      <p>
+        {timeToDate(project.startDate)} ~ {timeToDate(project.endDate)}
+      </p>
       <div className="link-container">
         {project.github && (
           <a className="link-wrap" href={project.github} target="_blank">
