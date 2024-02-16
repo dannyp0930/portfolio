5f954427b1fc917ea889a62650bab4ffa5d575cc
dannyp0930 <dannyp0930@onoffmix.com>
2024-02-17 05:03:18 +0900

    FEAT: banner image

--- src/components/pages/home/Banner.jsx
+++ src/components/pages/home/Banner.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import BannerDesktop from "assets/images/banner-desktop.webp";
+import BannerTablet from "assets/images/banner-tablet.webp";
+import BannerMobile from "assets/images/banner-mobile.webp";
 
 export default function Banner() {
+  console.log(BannerMobile);
   return (
     <section className="banner">
       <picture>
-        <source media="(min-width: 1280px)"/>
-        <source media="(min-width: 768px)" />
-        <img src="" alt="banner" />
+        <source media="(min-width: 1280px)" srcSet={BannerDesktop} />
+        <source media="(min-width: 768px)" srcSet={BannerTablet} />
+        <img src={BannerMobile} alt="banner" />
       </picture>
-      <h1>
-        박상훈
-        <br />
-        프론트엔드 엔지니어
-      </h1>
-      <p>
-        안녕하세요.
-        <br />
-        저는 계획을 세우는 것을 좋아합니다.
-        <br />
-        사용자가 어떤 기능을 필요로 하는지 고민합니다.
-      </p>
+      <div className="content">
+        <h1>
+          박상훈
+          <br />
+          프론트엔드 엔지니어
+        </h1>
+        <p>
+          안녕하세요.
+          <br />
+          저는 계획을 세우는 것을 좋아합니다.
+          <br />
+          사용자가 어떤 기능을 필요로 하는지 고민합니다.
+        </p>
+      </div>
     </section>
   );
 }
