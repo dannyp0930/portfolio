2d8a5be1c45c73795cc48fe435a7cbb6558d1322
dannyp0930 <dannyp0930@gmail.com>
2024-02-18 00:33:13 +0900

    FEAT: info component

--- src/components/pages/home/Info.jsx
+++ src/components/pages/home/Info.jsx
@@ -1,5 +1,122 @@
 import React from "react";
 
 export default function Info() {
-  return <section>Info</section>;
+  return (
+    <section className="info">
+      <div className="content-container">
+        <div className="content-wrap">
+          <h3>CONTACT</h3>
+          <ul>
+            <li>
+              <h4>E-mail</h4>
+              <p>
+                <a href="mailto:dannyp0930@gmail.com">dannyp0930@gmail.com</a>
+                <br />
+                <a href="mailto:dannyp0930@daum.net">dannyp0930@daum.net</a>
+              </p>
+            </li>
+            <li>
+              <h4>Phone</h4>
+              <p>
+                <a href="tel:010-8890-9708">010-8890-9708</a>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="content-wrap">
+          <h3>EDUCATION</h3>
+          <ul>
+            <li>
+              <h4>광주 금호고등학교</h4>
+              <p>2011.03 ~ 2014.02</p>
+            </li>
+            <li>
+              <h4>중앙대학교 기계공학부 기계공학전공</h4>
+              <p>2014.03 ~ 2020.02</p>
+            </li>
+          </ul>
+        </div>
+        <div className="content-wrap">
+          <h3>EXPERIENCE</h3>
+          <ul>
+            <li>
+              <h4>삼성청년SW아카데미(SSAFY)</h4>
+              <p>
+                2021.07 ~ 2022.06
+                <br />
+                프로젝트 중심의 학습 및 실습
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="content-wrap">
+          <h3>CARRER</h3>
+          <ul>
+            <li>
+              <h4>한국교통안전공단 인턴</h4>
+              <p>
+                2018.09.20 ~ 2018.12.31
+                <br />
+                서울본부안전사업2처
+                <br />
+                기계식주차장검사업무보조
+              </p>
+            </li>
+            <li>
+              <h4>온오프믹스</h4>
+              <p>
+                2023.02.22 ~
+                <br />
+                개발팀
+                <br />
+                프론트엔드엔지니어
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="content-wrap">
+          <h3>LANGUAGE</h3>
+          <ul>
+            <li>
+              <h4>TOEIC 885</h4>
+              <p>2023.11.12 YBM시사</p>
+            </li>
+            <li>
+              <h4>JLPT N1</h4>
+              <p>2021.08.09 국제교류기금</p>
+            </li>
+          </ul>
+        </div>
+        <div className="content-wrap">
+          <h3>CERTIFICATE</h3>
+          <ul>
+            <li>
+              <h4>한자능력검정시험</h4>
+              <p>2008.09.02 (사)한국어문회</p>
+            </li>
+            <li>
+              <h4>한국사능력검정시험</h4>
+              <p>2016.08.30 국사편찬위원회</p>
+            </li>
+            <li>
+              <h4>컴퓨터활용능력</h4>
+              <p>2017.04.07 대한상공회의소</p>
+            </li>
+            <li>
+              <h4>일반기계기사</h4>
+              <p>2018.08.17 한국산업인력공단</p>
+            </li>
+            <li>
+              <h4>SQLD</h4>
+              <p>2021.10.01 한국데이터산업진흥원</p>
+            </li>
+            <li>
+              <h4>정보처리기사</h4>
+              <p>2022.09.02 한국산업인력공단</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
