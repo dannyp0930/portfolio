99b51e11e88da41da88f6ee26c99056f43d98ac7
dannyp0930 <dannyp0930@gmail.com>
2024-09-14 17:05:43 +0900

    feat: career component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="w-dvw fixed top-0 left-0 z-50 h-header underline-offset-4 font-bold text-white">
-      <nav className="w-full h-full px-5 flex justify-between items-center md:px-12">
-        <a className="font-parisienne text-3xl md:text-4xl lg:text-5xl" href="#banner">SH
+    <header className="fixed top-0 left-0 z-50 font-bold text-white w-dvw h-header underline-offset-4">
+      <nav className="flex items-center justify-between w-full h-full px-5 md:px-12">
+        <a className="text-3xl font-parisienne md:text-4xl lg:text-5xl" href="#banner">SH
           <span className="hidden md:inline">&nbsp;Portfolio</span></a>
-        <ul className="hidden md:flex items-center gap-5 text-2xl">
+        <ul className="items-center hidden gap-5 text-2xl md:flex">
           <li><a className="hover:underline" href="#info">Info</a></li>
           <li><a className="hover:underline" href="#skills">Skills</a></li>
           <li><a className="hover:underline" href="#project">Project</a></li>
+          <li><a className="hover:underline" href="#career">Career</a></li>
         </ul>
       </nav>
     </header>
--- src/components/home/Career/index.jsx
+++ src/components/home/Career/index.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 
 export default function Career() {
+  const career_list = [
+    {
+      id: 1,
+      companyName: '온오프믹스',
+      position: '프론트엔드 개발자',
+      period: '2023.02 ~ (재직중)',
+      content: '종합 이벤트 비즈니스 플랫폼\n외주 프로젝트 작업 및 솔루션 개발',
+      detail: [
+        {
+          id: 1,
+          title: 'MICE 외주 프로젝트 서비스 개발 및 유지보수',
+          content: [
+            "회의, 컨벤션, 전시회 등의 산업 또는 행사 관련 서비스 프로젝트",
+            "Vue.js, React, Jquery 등을 사용한 Javascript 기반 프론트엔드 서비스 개발",
+            "Sass(SCSS), Tailwind CSS, Bootstrap 등을 사용한 CSS 스타일링",
+            "Google, Kakao, Naver OAuth 로그인",
+            "KCP 결제 모듈을 사용한 결제 서비스 구축",
+            "Nginx 파일 압축 전송 및 캐싱을 통한 성능 최적화",
+            "Docker 배포환경구축",
+          ]
+        }
+      ]
+    }
+    
+  ];
   return (
-    <section id="career" className="bg-white">
-      {/* Career */}
+    <section id="career" className="flex flex-col items-center justify-center gap-12 py-12 md:py-16 lg:py-28 bg-pale-beige">
+      <h1>Career</h1>
+      <ul className="flex flex-col gap-12">
+        {career_list.map(item => <li key={item.id}>
+          <h2>{item.companyName} <span className="text-sm">{item.period}</span></h2>
+          <h3 className="mt-3">{item.position}</h3>
+          <p className="mt-2 whitespace-pre-wrap">{item.content}</p>
+          <ul className="mt-10">
+            {item.detail.map(detail => <li key={detail.id}>
+              <h4>{detail.title}</h4>
+              <ul className="mt-5 list-disc list-inside">
+                {detail.content.map(content => <li key={content.id}>{content}</li>)}
+              </ul>
+            </li>)}
+          </ul>
+        </li>)}
+      </ul>
     </section>
   )
 }
