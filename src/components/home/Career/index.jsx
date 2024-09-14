ed13d0d6834e150a7a9a773edae96c5e8073b27f
dannyp0930 <dannyp0930@gmail.com>
2024-09-15 00:31:33 +0900

    feat: 경력 추가

--- src/components/home/Career/index.jsx
+++ src/components/home/Career/index.jsx
@@ -21,6 +21,18 @@ export default function Career() {
             "Nginx 파일 압축 전송 및 캐싱을 통한 성능 최적화",
             "Docker 배포환경구축",
           ]
+        },
+        {
+          id: 2,
+          title: "Web CMS(가칭)",
+          content: [
+            "Headless CMS(Payload CMS)를 사용한 웹 서비스 제작 솔루션",
+            "비개발자가 쉽게 홈페이지를 만들 수 있는 서비스",
+            "Backend + Frontend + DB + Proxy 인스턴스로 이루어진 서버 구성",
+            "React, Express.js 를 사용한 백엔드 서비스 개발",
+            "Nuxt.js를 사용한 프론트엔드 컴포넌트 개발",
+            "KCP 결제 모듈을 사용한 상품 결제 서비스",
+          ]
         }
       ]
     }
@@ -34,7 +46,7 @@ export default function Career() {
           <h2>{item.companyName} <span className="text-sm">{item.period}</span></h2>
           <h3 className="mt-3">{item.position}</h3>
           <p className="mt-2 whitespace-pre-wrap">{item.content}</p>
-          <ul className="mt-10">
+          <ul className="flex flex-col gap-5 mt-10">
             {item.detail.map(detail => <li key={detail.id}>
               <h4>{detail.title}</h4>
               <ul className="mt-5 list-disc list-inside">
