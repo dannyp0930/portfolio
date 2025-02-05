ac50b380420dfd0886c28440202194501276df1b
dannyp0930 <dannyp0930@gmail.com>
2025-02-05 22:09:55 +0900

    fix: type global

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,20 @@
 import { Timestamp } from "firebase/firestore"
 
-interface ProjectCardProps {
-  project: Project
+declare global {
+  interface ProjectCardProps {
+    project: Project
+  }
+  
+  interface Project {
+    title: string
+    intro: string
+    organization: string
+    startDate: Timestamp
+    endDate: Timestamp
+    github: string
+    homepage: string
+    notion: string
+  }
 }
 
-interface Project {
-  title: string
-  intro: string
-  organization: string
-  startDate: Timestamp
-  endDate: Timestamp
-  github: string
-  homepage: string
-  notion: string
-}
+export {}
