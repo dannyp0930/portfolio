62e44a4ad3a62948645b94a0d862692f31918ef2
dannyp0930 <dannyp0930@onoffmix.com>
2025-02-06 10:18:00 +0900

    refactor: typescript 전환 완료

---
+++ api/index.ts
@@ -0,0 +1,23 @@
+import { db } from "@/db";
+import { collection, doc, getDoc, getDocs, orderBy, OrderByDirection, query } from "firebase/firestore";
+
+const api = {
+  getDataList: async function (type: string, sortBy?: string, sortDir: string = "desc") {
+    let datas;
+    if (sortBy) {
+      datas = await getDocs(query(collection(db, type), orderBy(sortBy, sortDir as OrderByDirection)));
+    } else {
+      datas = await getDocs(query(collection(db, type)));
+    }
+    return datas.docs.map((doc) => {
+      return Object.assign(doc.data(), { id: doc.id });
+    });
+  },
+  getData: async function (type: string, id: string) {
+    const docRef = doc(db, type, id);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+  },
+};
+
+export default api;
