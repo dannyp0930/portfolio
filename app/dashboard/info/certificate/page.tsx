0e7a88c27f8aff16cebe2912da590cabf7ec83ae
dannyp0930 <dannyp0930@gmail.com>
2025-02-22 18:17:19 +0900

    feat: certificate

--- app/dashboard/info/certificate/page.tsx
+++ app/dashboard/info/certificate/page.tsx
@@ -1,5 +1,319 @@
 'use client';
 
+import instance from '@/app/api/instance';
+import AdminPagination from '@/components/dashboard/AdminPagination';
+import { Button } from '@/components/ui/button';
+import dayjs from 'dayjs';
+import { useSearchParams } from 'next/navigation';
+import { ChangeEvent, Fragment, MouseEvent, useEffect, useState } from 'react';
+
 export default function Certificate() {
-	return <div>Certificate</div>;
+	const searchParams = useSearchParams();
+	const [load, setLoad] = useState<boolean>(true);
+	const [certificateName, setCertificateName] = useState<string>('');
+	const [issueDate, setIssueDate] = useState<string>('');
+	const [issuingOrganization, setIssuingOrganization] = useState<string>('');
+	const [selectPage, setSelectPage] = useState<number>(1);
+	const [certificates, setCertificates] = useState<Certificate[]>([]);
+	const [totalCnt, setTotalCnt] = useState<number>(0);
+	const [updateCertificateId, setUpdateCertificateId] = useState<
+		number | null
+	>();
+	const [updateCertificate, setUpdateCertificate] =
+		useState<Certificate | null>();
+	const take = 20;
+
+	async function handleCreateCertificate(e: MouseEvent<HTMLButtonElement>) {
+		e.preventDefault();
+		setLoad(true);
+		try {
+			const body = {
+				certificateName,
+				issueDate: dayjs(issueDate).toDate(),
+				issuingOrganization,
+			};
+			const { data, status } = await instance.post(
+				'/api/info/certificate',
+				body
+			);
+			if (status === 200) {
+				alert(data.message);
+				setCertificateName('');
+				setIssueDate('');
+				setIssuingOrganization('');
+			}
+		} catch {
+			console.log(123);
+		} finally {
+			setLoad(false);
+		}
+	}
+
+	async function handleUpdateCertificate(e: MouseEvent<HTMLButtonElement>) {
+		e.preventDefault();
+		setLoad(true);
+		try {
+			const body = {
+				...updateCertificate,
+				issueDate: dayjs(updateCertificate?.issueDate).toDate(),
+			};
+			const { data, status } = await instance.put(
+				'/api/info/certificate',
+				body
+			);
+			if (status === 200) {
+				alert(data.message);
+				setUpdateCertificateId(null);
+				setUpdateCertificate(null);
+			}
+		} catch {
+			console.log(123);
+		} finally {
+			setLoad(false);
+		}
+	}
+
+	function handleDeleteCertificate(certificateId: number) {
+		return async (e: MouseEvent<HTMLButtonElement>) => {
+			e.preventDefault();
+			setLoad(true);
+			try {
+				const body = { id: certificateId };
+				const { data, status } = await instance.delete(
+					'/api/info/certificate',
+					{ data: body }
+				);
+				if (status === 200) {
+					alert(data.message);
+					setUpdateCertificateId(null);
+					setUpdateCertificate(null);
+				}
+			} catch {
+				console.log(123);
+			} finally {
+				setLoad(false);
+			}
+		};
+	}
+
+	function selectUpdateCertificate(certificate?: Certificate) {
+		return (e: MouseEvent<HTMLButtonElement>) => {
+			e.preventDefault();
+			if (certificate) {
+				setUpdateCertificateId(certificate.id);
+				setUpdateCertificate(certificate);
+			} else {
+				setUpdateCertificateId(null);
+				setUpdateCertificate(null);
+			}
+		};
+	}
+
+	function changeSelectUpdateCertificate(key: keyof Certificate) {
+		return (e: ChangeEvent<HTMLInputElement>) => {
+			setUpdateCertificate({
+				...updateCertificate,
+				[key]: e.target.value,
+			} as Certificate);
+		};
+	}
+
+	useEffect(() => {
+		async function getCertificate() {
+			const params = {
+				page: selectPage,
+				take,
+			};
+			try {
+				const {
+					data: { data, totalCnt },
+				} = await instance.get('/api/info/certificate', { params });
+				setCertificates(data);
+				setTotalCnt(totalCnt);
+				setLoad(false);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getCertificate();
+	}, [load, selectPage]);
+
+	useEffect(() => {
+		const parmasPage = searchParams.get('page');
+		if (parmasPage) {
+			setSelectPage(parseInt(parmasPage));
+		}
+	}, [searchParams]);
+
+	return (
+		<div className="py-10 rounded-lg bg-white">
+			<table className="w-full border-collapse table-fixed [&_th]:border [&_th]:px-2 [&_th]:py-1 [&_td]:border [&_td]:px-2 [&_td]:py-1 [&_td]:overflow-auto">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>자격</th>
+						<th>취득일자</th>
+						<th>주관</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td>
+							<input
+								className="w-full focus:outline-none"
+								type="text"
+								value={certificateName}
+								required
+								onChange={(e) =>
+									setCertificateName(e.target.value)
+								}
+							/>
+						</td>
+						<td>
+							<input
+								className="w-full focus:outline-none"
+								type="date"
+								value={issueDate}
+								required
+								onChange={(e) => setIssueDate(e.target.value)}
+							/>
+						</td>
+						<td>
+							<input
+								className="w-full focus:outline-none"
+								type="text"
+								value={issuingOrganization}
+								required
+								onChange={(e) =>
+									setIssuingOrganization(e.target.value)
+								}
+							/>
+						</td>
+						<td>
+							<div className="flex justify-center">
+								<Button
+									size="sm"
+									onClick={handleCreateCertificate}
+								>
+									추가
+								</Button>
+							</div>
+						</td>
+					</tr>
+					{certificates.map((certificate) => (
+						<tr
+							key={certificate.id}
+							className={
+								certificate.id === updateCertificateId
+									? 'ring-inset ring-2 ring-theme-sub'
+									: ''
+							}
+						>
+							{certificate.id === updateCertificateId ? (
+								<Fragment>
+									<td>{certificate.id}</td>
+									<td>
+										<input
+											className="w-full focus:outline-none"
+											onChange={changeSelectUpdateCertificate(
+												'certificateName'
+											)}
+											type="text"
+											value={
+												updateCertificate?.certificateName
+											}
+										/>
+									</td>
+									<td>
+										<input
+											className="w-full focus:outline-none"
+											onChange={changeSelectUpdateCertificate(
+												'issueDate'
+											)}
+											type="date"
+											value={dayjs(
+												updateCertificate?.issueDate
+											).format('YYYY-MM-DD')}
+										/>
+									</td>
+									<td>
+										<input
+											className="w-full focus:outline-none"
+											onChange={changeSelectUpdateCertificate(
+												'issuingOrganization'
+											)}
+											type="text"
+											value={
+												updateCertificate?.issuingOrganization
+											}
+										/>
+									</td>
+									<td>
+										<div className="flex gap-2 justify-center">
+											<Button
+												size="sm"
+												onClick={
+													handleUpdateCertificate
+												}
+											>
+												저장
+											</Button>
+											<Button
+												variant="secondary"
+												size="sm"
+												onClick={selectUpdateCertificate()}
+											>
+												취소
+											</Button>
+										</div>
+									</td>
+								</Fragment>
+							) : (
+								<Fragment>
+									<td>{certificate.id}</td>
+									<td>{certificate.certificateName}</td>
+									<td>
+										{dayjs(certificate.issueDate).format(
+											'YYYY.MM.DD'
+										)}
+									</td>
+									<td>{certificate.issuingOrganization}</td>
+									<td>
+										<div className="flex gap-2 justify-center">
+											<Button
+												size="sm"
+												onClick={selectUpdateCertificate(
+													certificate
+												)}
+											>
+												수정
+											</Button>
+											<Button
+												variant="destructive"
+												size="sm"
+												onClick={handleDeleteCertificate(
+													certificate.id
+												)}
+											>
+												삭제
+											</Button>
+										</div>
+									</td>
+								</Fragment>
+							)}
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<AdminPagination
+				className="mt-10"
+				page={selectPage}
+				totalCnt={totalCnt}
+				take={take}
+				size={5}
+			/>
+		</div>
+	);
 }
