16ef03ac6444ca49b40dc4593a85c92853dbc3a5
dannyp0930 <dannyp0930@gmail.com>
2025-03-31 18:01:31 +0900

    feat: profile page

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -98,8 +98,8 @@ export default async function Header() {
 						</a>
 					</li>
 					{session ? (
-						<li>
-							{isAdmin === 'true' ? (
+						isAdmin === 'true' ? (
+							<li>
 								<Link
 									className="hover:underline"
 									href="dashboard"
@@ -108,12 +108,9 @@ export default async function Header() {
 										Dashboard
 									</span>
 									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth="1.5"
-										stroke="currentColor"
 										className="w-6 h-6 stroke-2 md:hidden stroke-white fill-transparent"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
 									>
 										<path
 											strokeLinecap="round"
@@ -127,10 +124,35 @@ export default async function Header() {
 										/>
 									</svg>
 								</Link>
-							) : (
-								<LogoutButton />
-							)}
-						</li>
+							</li>
+						) : (
+							<>
+								<li>
+									<Link
+										className="hover:underline"
+										href="profile"
+									>
+										<span className="hidden md:inline">
+											Profile
+										</span>
+										<svg
+											className="w-6 h-6 stroke-2 md:hidden stroke-white fill-transparent"
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+											/>
+										</svg>
+									</Link>
+								</li>
+								<li>
+									<LogoutButton />
+								</li>
+							</>
+						)
 					) : (
 						<li>
 							<Link className="hover:underline" href="login">
