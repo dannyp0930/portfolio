860147ebb638a0f6adaefd550d0e8ac08bbb680c
dannyp0930 <dannyp0930@gmail.com>
2025-04-01 18:07:53 +0900

    fix: dashboard header footer 숨김처리

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from '@/components/ui/sonner';
 import AuthProvider from '@/components/common/AuthProvider';
 import Header from '@/components/Layout/Header';
 import Footer from '@/components/Layout/Footer';
+import { headers } from 'next/headers';
 
 export const metadata: Metadata = {
 	title: 'SH Portfolio',
@@ -11,19 +12,23 @@ export const metadata: Metadata = {
 	manifest: '/manifest.json',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	// todo: client component 분리
+	const headersList = await headers();
+	const pathname = new URL(headersList.get('referer') || '/').pathname;
+	const isDashboard = pathname.startsWith('/dashboard');
 	return (
 		<html lang="en">
 			<body className="bg-peach-fuzz">
 				<AuthProvider>
 					<main>
-						<Header />
+						{!isDashboard && <Header />}
 						{children}
-						<Footer />
+						{!isDashboard && <Footer />}
 					</main>
 					<Toaster richColors />
 				</AuthProvider>
