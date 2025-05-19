7ffb23806563e6f50f3aa923f78ea8383700a149
dannyp0930 <dannyp0930@gmail.com>
2025-05-19 16:09:25 +0900

    refactor

--- app/privacy-policy/newsletter/page.tsx
+++ app/privacy-policy/newsletter/page.tsx
@@ -3,8 +3,10 @@ import { Fragment } from 'react';
 
 export default function Newsletter() {
 	return (
-		<article className="min-h-[calc(100dvh-theme(spacing.footer))] py-header px-20">
-			<h1>{newsletterPolicy.title}</h1>
+		<article className="min-h-[calc(100dvh-theme(spacing.footer))] py-header px-5 md:px-10 lg:w-[1024px] lg:m-auto">
+			<h1 className="text-2xl text-center lg:text-3xl">
+				{newsletterPolicy.title}
+			</h1>
 			<div className="flex flex-col gap-4 mt-5">
 				{newsletterPolicy.sections.map((policy, i) => (
 					<div key={`policy-${i}`}>
--- app/privacy-policy/registration/page.tsx
+++ app/privacy-policy/registration/page.tsx
@@ -3,8 +3,10 @@ import { Fragment } from 'react';
 
 export default function Registration() {
 	return (
-		<article className="min-h-[calc(100dvh-theme(spacing.footer))] py-header px-20">
-			<h1>{registrationPolicy.title}</h1>
+		<article className="min-h-[calc(100dvh-theme(spacing.footer))] py-header px-5 md:px-10 lg:w-[1024px] lg:m-auto">
+			<h1 className="text-2xl text-center lg:text-3xl">
+				{registrationPolicy.title}
+			</h1>
 			<div className="flex flex-col gap-4 mt-5">
 				{registrationPolicy.sections.map((policy, i) => (
 					<div key={`policy-${i}`}>
