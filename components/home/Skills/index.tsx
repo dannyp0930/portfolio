d26b588d789b8d1ed74febda372f05198c145f12
dannyp0930 <dannyp0930@gmail.com>
2025-05-23 17:24:58 +0900

    refactor: 스타일 변경

--- components/home/Skills/index.tsx
+++ components/home/Skills/index.tsx
@@ -16,10 +16,10 @@ export default function Skills({ skills }: SkillsProps) {
 						<div className="grid items-center grid-cols-4 gap-4 lg:grid-cols-6 md:grid-cols-5 md:gap-12">
 							{skills.map((skill) => (
 								<div
-									className="relative transition-transform cursor-pointer hover:-translate-y-5 group"
+									className="relative transition-transform cursor-pointer hover:-translate-y-5 group bg-white p-4 rounded"
 									key={skill.id}
 								>
-									<div className="absolute top-0 z-50 hidden w-40 p-3 max-h-40 md:max-h-auto overflow-y-auto -translate-x-1/2 bg-white border border-solid rounded-lg skill-detail border-theme-sub md:w-48 lg:w-72 left-1/2 md:p-5 hover:md:-translate-y-5 group-hover:block group-hover:-translate-y-[calc(100%+5px)] md:group-hover:-translate-y-[calc(100%+20px)]">
+									<div className="absolute top-0 z-50 hidden w-40 p-1 max-h-40 md:max-h-auto overflow-y-auto -translate-x-1/2 bg-white border border-solid rounded-lg skill-detail border-theme-sub md:w-48 lg:w-72 left-1/2 md:p-5 hover:md:-translate-y-5 group-hover:block group-hover:-translate-y-[calc(100%+5px)] md:group-hover:-translate-y-[calc(100%+20px)]">
 										<h3 className="text-lg md:text-2xl">
 											{skill.title}
 										</h3>
@@ -48,6 +48,7 @@ export default function Skills({ skills }: SkillsProps) {
 										src={skill.imageUrl}
 										alt={skill.title}
 									/>
+									<p className="mt-5">{skill.title}</p>
 								</div>
 							))}
 						</div>
