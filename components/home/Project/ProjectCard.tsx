3e113a9972d224f2e0cf6eb7fe6c79f767e32201
dannyp0930 <dannyp0930@gmail.com>
2025-06-09 16:28:00 +0900

    feat: project card

--- components/home/Project/ProjectCard.tsx
+++ components/home/Project/ProjectCard.tsx
@@ -8,67 +8,73 @@ import dayjs from 'dayjs';
 
 export default function ProjectCard({ project, setModalId }: ProjectCardProps) {
 	return (
-		<div className="box-border flex flex-col justify-center w-full gap-5 p-5 bg-white h-full rounded-xl">
+		<div className="box-border flex flex-col justify-between gap-4 w-full p-5 bg-white h-full rounded-xl">
 			<h1 className="flex flex-col flex-wrap md:flex-row lg:justify-between lg:items-center">
 				{project.title} {project.hasProjectDetail}
 				<span className="text-base">{project.organization}</span>
 			</h1>
-			<h4 className="break-keep">{project.intro}</h4>
-			<p>
-				{dayjs(project.startDate).format('YYYY.MM.DD')} ~{' '}
-				{project.endDate && dayjs(project.endDate).format('YYYY.MM.DD')}
-			</p>
-			<div className="flex items-center gap-5 mt-3 xl:gap-12">
-				{project.github && (
-					<a
-						className="w-12 h-12 text-theme-sub"
-						href={project.github}
-						target="_blank"
-					>
-						<FontAwesomeIcon
-							className="w-full h-full"
-							icon={faGithub}
-						/>
-					</a>
-				)}
-				{project.homepage && (
-					<a
-						className="w-12 h-12 text-theme-sub"
-						href={project.homepage}
-						target="_blank"
-					>
-						<FontAwesomeIcon
-							className="w-full h-full"
-							icon={faHome}
-						/>
-					</a>
-				)}
-				{project.notion && (
-					<a
-						className="w-12 h-12 text-theme-sub"
-						href={project.notion}
-						target="_blank"
-					>
-						<Image
-							className="w-full h-full"
-							width={150}
-							height={150}
-							src={notionSvg.src}
-							alt="notion"
-						/>
-					</a>
-				)}
-				{project.hasProjectDetail && (
-					<button
-						className="w-12 h-12 text-theme-sub"
-						onClick={() => setModalId(project.id)}
-					>
-						<FontAwesomeIcon
-							className="w-full h-full"
-							icon={faCircleInfo}
-						/>
-					</button>
-				)}
+			<div className="flex flex-col justify-end gap-4">
+				<h4 className="break-all">
+					{project.intro}
+					<br />
+					<span className="text-base font-normal">
+						{dayjs(project.startDate).format('YYYY.MM.DD')} ~{' '}
+						{project.endDate &&
+							dayjs(project.endDate).format('YYYY.MM.DD')}
+					</span>
+				</h4>
+				<div className="flex items-center gap-5 xl:gap-12">
+					{project.github && (
+						<a
+							className="w-12 h-12 text-theme-sub"
+							href={project.github}
+							target="_blank"
+						>
+							<FontAwesomeIcon
+								className="w-full h-full"
+								icon={faGithub}
+							/>
+						</a>
+					)}
+					{project.homepage && (
+						<a
+							className="w-12 h-12 text-theme-sub"
+							href={project.homepage}
+							target="_blank"
+						>
+							<FontAwesomeIcon
+								className="w-full h-full"
+								icon={faHome}
+							/>
+						</a>
+					)}
+					{project.notion && (
+						<a
+							className="w-12 h-12 text-theme-sub"
+							href={project.notion}
+							target="_blank"
+						>
+							<Image
+								className="w-full h-full"
+								width={150}
+								height={150}
+								src={notionSvg.src}
+								alt="notion"
+							/>
+						</a>
+					)}
+					{project.hasProjectDetail && (
+						<button
+							className="w-12 h-12 text-theme-sub"
+							onClick={() => setModalId(project.id)}
+						>
+							<FontAwesomeIcon
+								className="w-full h-full"
+								icon={faCircleInfo}
+							/>
+						</button>
+					)}
+				</div>
 			</div>
 		</div>
 	);
